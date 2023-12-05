6bfdba66e8b1a9bcca35f6713107edb0e2c506e4
maxhaesslein <git@maxhaesslein.de>
2023-12-05 09:24:24 +0100

    day 05, part 2

--- day05/answer.js
+++ day05/answer.js
@@ -3,32 +3,25 @@ function prepareInput( input ) {
 
 	input = input.split("\n\n");
 
-	var seeds = input[0].split(':')[1].trim().split(' ');
-	var seed2soil = input[1].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var soil2fertilizer = input[2].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var fertilizer2water = input[3].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var water2light = input[4].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var light2temperature = input[5].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var temperature2humidity = input[6].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	var humidity2location = input[7].split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');});
-	
+	var seeds = input.shift().split(':')[1].trim().split(' ');
+
+	var transforms = [];
+	for( var transform of input ) {
+		transform = transform.split(':\n')[1].trim().split("\n").map(function(i){return i.trim().split(' ');})
+		transforms.push(transform);
+	}
+
 	var almanac = {
 		'seeds': seeds,
-		'seed2soil': seed2soil,
-		'soil2fertilizer': soil2fertilizer,
-		'fertilizer2water': fertilizer2water,
-		'water2light': water2light,
-		'light2temperature': light2temperature,
-		'temperature2humidity': temperature2humidity,
-		'humidity2location': humidity2location
+		'transforms': transforms,
 	};
 
 	return almanac;
 }
 
-function map( map, input ) {
+function map_one( transform, input ) {
 
-	for( var range of map ) {
+	for( var range of transform ) {
 
 		var destinationStart = parseInt(range[0], 10),
 			sourceStart = parseInt(range[1], 10),
@@ -45,33 +38,94 @@ function map( map, input ) {
 	return input;
 }
 
-function getLocation( seed, almanac ) {
-
-	seed = parseInt(seed, 10);
-	
-	var soil = map(almanac['seed2soil'], seed ),
-		fertilizer = map(almanac['soil2fertilizer'], soil),
-		water = map(almanac['fertilizer2water'], fertilizer),
-		light = map(almanac['water2light'], water),
-		temperature = map(almanac['light2temperature'], light),
-		humidity = map(almanac['temperature2humidity'], temperature),
-		location = map(almanac['humidity2location'], humidity);
-
-	return location;
-}
-
 function answer1( almanac ) {
 
 	var closestLocation = Infinity;
 
 	for( var seed of almanac['seeds'] ) {
-		var location = getLocation( seed, almanac );
-		if( location < closestLocation ) closestLocation = location;
+		step = parseInt(seed, 10);
+
+		for( var transform of almanac['transforms'] ) {
+			step = map_one(transform, step);
+		}
+
+		if( step < closestLocation ) closestLocation = step;
 	}
 
 	return closestLocation;
 }
 
+function map_range( transform, input ) {
+
+	var restList = input;
+
+	var answerList = [];
+
+	for( var range of transform ) {
+
+		var destinationStart = parseInt(range[0], 10),
+			sourceStart = parseInt(range[1], 10),
+			rangeLength = parseInt(range[2], 10);
+
+		sourceEnd = sourceStart+rangeLength;
+
+		var testAgainList = [];
+
+		while( restList.length ){
+
+			var currentRange = restList.pop(),
+				currentRangeStart = currentRange[0],
+				currentRangeEnd = currentRange[1];
+
+			var before = [currentRangeStart, Math.min(currentRangeEnd, sourceStart)],
+				middle = [Math.max(currentRangeStart, sourceStart), Math.min(sourceEnd, currentRangeEnd)],
+				after = [Math.max(sourceEnd, currentRangeStart), currentRangeEnd];
+
+			if( before[1] > before[0] ) {
+				testAgainList.push(before);
+			}
+			if( middle[1] > middle[0] ) {
+				answerList.push( [ middle[0]-sourceStart+destinationStart, middle[1]-sourceStart+destinationStart ] );
+			}
+			if( after[1] > after[0] ) {
+				testAgainList.push(after);
+			}
+
+		}
+
+		restList = testAgainList;
+
+	}
+
+	return answerList.concat(restList);
+}
+
 function answer2( almanac ) {
-	return '?';
+
+	var closestLocations = [];
+
+	var seeds = almanac['seeds'];
+	for( var i = 0; i < seeds.length; i++ ) {
+
+		var seedStart = parseInt(seeds[i], 10),
+			seedLength = parseInt(seeds[++i], 10);
+
+		var range = [[seedStart,seedStart+seedLength]];
+
+		for( var transform of almanac['transforms'] ) {
+			range = map_range(transform, range);
+		}
+
+		var closestLocation = Infinity;
+		for( var currentRange of range ) {
+			if( currentRange[0] < closestLocation ) closestLocation = currentRange[0];
+		}
+
+		closestLocations.push( closestLocation );
+
+	}
+
+	closestLocations.sort(function(a,b){return a-b;});
+
+	return closestLocations[0];
 }
